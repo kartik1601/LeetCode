6ceea2e645c173d7359d363550489c1e84d2654b
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-01-17 13:37:31 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (97.68%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> mp;
-        for(int num:arr){mp[num]++;}
+        int mp[2001]={0};
+        for(int num:arr){mp[num+1000]++;}
         int freq[1001]={0};
-        for(auto&[n,c]:mp){if(freq[c]==1){return false;}; freq[c]=1;}
+        for(int c:mp){if(c!=0 && freq[c]==1){return false;}; freq[c]=1;}
         return true;
     }
 };
