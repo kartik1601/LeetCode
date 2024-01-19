f254e4a88b7ccfe1f192c4ffcbb404f4f27bb0ea
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-01-19 13:09:44 +0530

    Time: 15 ms (39.09%), Space: 10.5 MB (83.40%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& mat) {
         int n = mat.size();
-        vector<vector<int>> dp(n+1,vector<int>(n,INT_MAX));
-        for(int i=0 ; i<n ; i++){dp[n][i]=0;}
+        vector<int> curr(n,INT_MAX), next(n,0);
         int ans=INT_MAX;
         for(int r=n-1 ; r>=0; r--){
             for(int c=0 ; c<n ; c++){
                 int res=INT_MAX;
-                if(c-1>=0){res=min(res,dp[r+1][c-1]);}
-                if(c  >=0){res=min(res,dp[r+1][c]);}
-                if(c+1< n){res=min(res,dp[r+1][c+1]);}
-                dp[r][c] = mat[r][c] + res;
-                if(!r){ans = min(ans,dp[r][c]);}
+                if(c-1>=0){res=min(res,next[c-1]);}
+                if(c  >=0){res=min(res,next[c]);}
+                if(c+1< n){res=min(res,next[c+1]);}
+                curr[c] = mat[r][c] + res;
+                if(!r){ans = min(ans,curr[c]);}
             }
+            next=curr;
         }
         
         return ans;
