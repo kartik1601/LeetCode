c83add142aceb75d2872f6d97a6e817fe94b3774
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-02-08 20:30:31 +0530

    Time: 81 ms (32.16%), Space: 19.9 MB (6.80%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,18 +4,15 @@ public:
         int n=nums.size();
         
         vector<long long> dp(n,INT_MAX);
+        dp[n-1]=0;
         
-        return memo(0,nums,dp);
-    }
-    int memo(int idx,vector<int>&nums,vector<long long>&dp){
-        if(idx==nums.size()-1){return 0;}
-        if(nums[idx]==0){return dp[idx];}
-        if(dp[idx]!=INT_MAX){return dp[idx];}
-        
-        for(int j=nums[idx] ; j>0 ; j--){
-            if(idx+j>nums.size()-1){continue;}
-            dp[idx] = min(dp[idx],1LL+memo(idx+j,nums,dp));
+        for(int i=n-2 ; i>=0 ; i--){
+            for(int j=nums[i] ; j>0 ; j--){
+                if(i+j>n-1){continue;}
+                dp[i] = min(dp[i],1+dp[i+j]);
+            }
         }
-        return dp[idx];
+        
+        return dp[0];
     }
 };
