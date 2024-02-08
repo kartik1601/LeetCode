c9a134627bdcb2d9eff6c8bda5e606e5bd56d1d4
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-02-08 14:10:27 +0530

    Time: 42 ms (89.24%), Space: 10.5 MB (32.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int numSquares(int n) {
         vector<int> dp(n+1,INT_MAX);
-        return memo(n,dp);
-    }
-    int memo(int n,vector<int>&dp){
-        if(n<=0){return 0;}
-        if(dp[n]!=INT_MAX){return dp[n];}
-        
-        for(int i=1 ; i*i<=n ; i++){
-            dp[n]=min(dp[n],1+memo(n-i*i,dp));
+        dp[0]=0;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j*j<=i ; j++){
+                dp[i] = min(dp[i],1+dp[i-j*j]);
+            }
         }
         return dp[n];
     }
