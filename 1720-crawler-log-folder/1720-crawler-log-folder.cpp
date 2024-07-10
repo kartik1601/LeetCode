c72e11f79ee7290b90c5b53dbe723c4007d76238
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-07-10 14:35:32 +0530

    Time: 6 ms (40.12%), Space: 13.9 MB (61.11%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int op=0;
+
+        for(auto&log:logs){
+            if(log=="../"){
+                if(op>0){op--;}
+            } else if (log=="./"){
+                op = op;
+            } else {
+                op++;
+            }
+        }
+
+        return op;
+    }
+};
