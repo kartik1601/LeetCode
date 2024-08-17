053844445f77c234d6f0518ec2776170dcbfc2e5
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-08-18 03:34:59 +0530

    Time: 99 ms (74.22%), Space: 51.5 MB (74.3%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& arr) {
-        vector<int> nums = arr;
-        int n=nums.size(), l=0 , r=1, maxLen=1;
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size(),maxLen=1;
         if(!n){return 0;}
-        sort(nums.begin(),nums.end());
-        nums.resize(unique(nums.begin(),nums.end())-nums.begin());
+        priority_queue<int,vector<int>,greater<int>> pq(nums.begin(),nums.end());
 
-        while(r<n){
-            while(r<n && nums[r-1]+1==nums[r]){r++;}
-            maxLen = max(maxLen,r-l);
-            l=r; r++;
+        int cnt=0,prev=pq.top()-1;
+        while(!pq.empty()){
+            if(!pq.empty() && prev==pq.top()){pq.pop();}
+            else if(!pq.empty() && prev+1 == pq.top()){
+                prev = pq.top(); pq.pop();
+                cnt++;
+            } else {
+                cnt=0; prev = pq.empty() ? 0 : pq.top()-1;
+            }
+            maxLen = max(maxLen,cnt);
         }
 
         return maxLen;
