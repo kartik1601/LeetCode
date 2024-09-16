c152d6d593ca685a0fd9827af5354d26158e9d58
Kartik Upadhyay <98540770+kartik1601@users.noreply.github.com>
2024-09-16 17:06:56 +0530

    Time: 32 ms (69.61%), Space: 25.1 MB (11.44%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.cpp
+++ 0838-design-linked-list/0838-design-linked-list.cpp
@@ -4,11 +4,18 @@
         ListNode* next;
         ListNode(int v) : val(v) , next(NULL) {}
         ListNode(int v, ListNode*n) : val(v) , next(n) {}
-    }
+    };
 */
+struct ListNodeD{
+    int val;
+    ListNodeD* next;
+    ListNodeD* prev;
+    ListNodeD(int v) : val(v) , next(NULL) , prev(NULL) {}
+    ListNodeD(int v, ListNodeD*n, ListNodeD*p) : val(v) , next(n) , prev(p) {}
+};
 class MyLinkedList {
     int size;
-    ListNode *head, *tail;
+    ListNodeD *head, *tail;
 public:
     MyLinkedList() {
         size = 0;
@@ -18,17 +25,29 @@ public:
     
     int get(int index) { // O(index)
         if(index >= size){return -1;}
-        ListNode *curr = head;
-        while(curr && index){
-            curr = curr->next;
-            index -= 1;
+
+        if(index <= size/2){
+            ListNodeD *curr = head;
+            while(curr && index){
+                curr = curr->next;
+                index -= 1;
+            }
+            return curr->val;
+        } else {
+            ListNodeD *curr = tail;
+            index = size - index-1;
+            while(curr && index){
+                curr = curr->prev;
+                index -= 1;
+            }
+            return curr->val;
         }
-        return curr->val;
     }
     
     void addAtHead(int val) { // O(1)
-        ListNode *temp = new ListNode(val);
+        ListNodeD *temp = new ListNodeD(val);
         temp->next = head;
+        if(head){head->prev = temp;}
         head = temp;
         if(!size){tail = head;}
         size++;
@@ -38,8 +57,11 @@ public:
         if(!size){
             addAtHead(val); return;
         }
-        ListNode *temp = new ListNode(val);
-        if(tail){tail->next = temp;}
+        ListNodeD *temp = new ListNodeD(val);
+        if(tail){
+            tail->next = temp;
+            temp->prev = tail;
+        }
         tail = temp;
         size++;
     }
@@ -53,17 +75,31 @@ public:
             return;
         }
 
-        ListNode *curr = head, *prev = NULL;
+        if(index <= size/2){
+            ListNodeD *curr = head, *prev = NULL;
+            while(curr && index){
+                prev = curr;
+                curr = curr->next;
+                index -= 1;
+            }
 
-        while(curr && index){
-            prev = curr;
-            curr = curr->next;
-            index -= 1;
+            ListNodeD *temp = new ListNodeD(val,curr,prev);
+            curr->prev = temp;
+            prev->next = temp;
+        } else {
+            index = size - index;
+            ListNodeD *curr = tail, *prev=NULL;
+            while(curr && index){
+                prev = curr;
+                curr = curr->prev;
+                index -= 1;
+            }
+
+            ListNodeD *temp = new ListNodeD(val,prev,curr);
+            prev->prev = temp;
+            curr->next = temp;
         }
 
-        ListNode *temp = new ListNode(val);
-        temp->next = curr;
-        prev->next = temp;
         size++;
     }
     
@@ -72,26 +108,55 @@ public:
             return;
         }
         if(index == 0){
-            ListNode *temp = head;
+            ListNodeD *temp = head;
             head = head->next;
+            if(head){head->prev = NULL;}
             temp->next = NULL;
             delete temp;
             size--;
             return;
         }
 
-        ListNode *curr = head, *prev = NULL;
-        int idx = index;
-        while(curr && idx){
-            prev = curr;
-            curr = curr->next;
-            idx -= 1;
+        if(index == size-1){
+            ListNodeD *temp = tail;
+            tail = tail->prev;
+            tail->next = NULL;
+            temp->prev = NULL;
+            delete temp;
+            size--;
+            return;
         }
 
-        prev->next = curr->next;
-        if(index==size-1){tail = prev;}
-        curr->next = NULL;
-        delete curr;
+        if(index <= size/2){
+            ListNodeD *curr = head, *prev = NULL;
+            int idx = index;
+            while(curr && idx){
+                prev = curr;
+                curr = curr->next;
+                idx -= 1;
+            }
+
+            prev->next = curr->next;
+            curr->next->prev = prev;
+            curr->next = NULL;
+            curr->prev = NULL;
+            delete curr;
+        } else {
+            ListNodeD *curr = tail, *prev = NULL;
+            int idx = size - index-1;
+            while(curr && idx){
+                prev = curr;
+                curr = curr->prev;
+                idx -= 1;
+            }
+
+            if(prev){prev->prev = curr->prev;}
+            curr->prev->next = prev;
+            curr->next = NULL;
+            curr->prev = NULL;
+            delete curr;
+        }
+
         size--;
     }
 };
